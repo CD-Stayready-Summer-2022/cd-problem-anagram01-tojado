d57608c64635879e79d9ce6063b747326fd6228e
Tory Doby <torydoby43@gmail.com>
2022-08-03 16:53:46 -0400

    updates

--- src/main/java/com/codedifferently/Anagram01.java
+++ src/main/java/com/codedifferently/Anagram01.java
@@ -1,9 +1,41 @@
 package com.codedifferently;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Anagram01 {
 
-    public static String[] problem(String inputWord, String[] inputWordList){
-        return null;
+    public static String[] problem(String inputWord, String[] inputWordList) {
+        Map<Character, Integer> inputMap = new HashMap<>();
+        ArrayList<String> anagrams = new ArrayList<>(0);
+        for (int i = 0; i < inputWord.length(); i++) {
+            Character current = inputWord.charAt(i);
+            int count = inputMap.getOrDefault(current, 0);
+            inputMap.put(current, ++count);
+        }
+      //  ArrayList<String> anagrams = null;
+        for (String word : inputWordList) {
+            Map<Character, Integer> wordMap = new HashMap<>();
+            for (int i = 0; i < word.length(); i++) {
+                Character current = word.charAt(i);
+                int count = wordMap.getOrDefault(current, 0);
+                wordMap.put(current, ++count);
+            }
+            boolean flag = false;
+            for (Character character : wordMap.keySet()) {
+                Integer inputFrequency = inputMap.get(character);
+                Integer wordFrequency = wordMap.get(character);
+                if (inputFrequency == null) {
+                    flag = false;
+                    break;
+                } else if (wordFrequency <= inputFrequency) {
+                    flag = true;
+
+                }
+            }
+            if (flag) anagrams.add(word);
+        }
+
+        return anagrams.toArray(new String[0]);
     }
 }
